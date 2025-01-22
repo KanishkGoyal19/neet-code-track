c012c498360563f3a1650abb9062ed3c088adf4b
Kanishk Goyal <159497810+KanishkGoyal19@users.noreply.github.com>
2025-01-22 14:06:13 -0500

    Update #001-contains-duplicate.java

--- Arrays_and_Hashing/#001-contains-duplicate.java
+++ Arrays_and_Hashing/#001-contains-duplicate.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for(int num : nums){
+            if(seen.contains(num)){
+                return true;
+            }
+            seen.add(num);
+        } 
+        return false;
+    }
+}
